640ec12c2371df291d0bf66a2db3f3f760467b9d
Tristan <tristanshaw000@gmail.com>
2025-07-02 22:25:52 -0400

    fix(renderer)

--- src/client/java/dev/thoq/utilities/misc/RyeConstants.java
+++ src/client/java/dev/thoq/utilities/misc/RyeConstants.java
@@ -18,7 +18,7 @@ package dev.thoq.utilities.misc;
 
 public class RyeConstants {
     public static final String NAME = "Rye";
-    public static final String VERSION = "0.1.15";
+    public static final String VERSION = "0.1.18";
     public static final String KIND = "Beta";
-    public static final String BUILD_NUMBER = "712025.8";
+    public static final String BUILD_NUMBER = "070225.3";
 }
